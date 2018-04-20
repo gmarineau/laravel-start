eaf07d9d2aaf4a0a31e1ef30220970c8aaacedff
Greg Marineau <greg.marineau@devfactory.ch>
2018-04-20 16:00:15 +0200

    Remove some old package

--- config/app.php
+++ config/app.php
@@ -185,8 +185,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\MacroServiceProvider::class,
-
     ],
 
     /*
